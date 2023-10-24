18fb4b253935654dd92254b821787e639e150f52
neapolicy <matthewlin559@gmail.com>
2023-10-24 15:37:24 -0400

    Changes by matthew lin

--- src/Wetworks.java
+++ src/Wetworks.java
@@ -3,9 +3,12 @@ public class Wetworks
     private int accuracy;
     private int health;
     private int stamina;
+    private int damageReduction;
 
     public Wetworks()
     {
         System.out.println("Sticking out your gyat for the rizzler. You're so skibidi. You're so fanum tax. I just wanna be your sigma.");
     }
 }
+
+//
