3c7569ebb0bd809cbe13db1deee18bd7e8c11b78
Neapolicy <matthewlin559@gmail.com>
2023-10-24 19:05:50 -0400

    fixed a minor bug with slots where if you just rolled 7 three times across multiple attempts you would just win

--- src/Daler.java
+++ src/Daler.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Daler extends Character
 {
+    private int health;
+    int moves;
     private final Scanner s = new Scanner(System.in);
     public Daler(int health, int stamina)
     {
@@ -11,5 +13,32 @@ public class Daler extends Character
     {
         System.out.print("Your hp is " + getHealth());
         System.out.print("Your stamina is " + getStamina());
+    }
+    public void choice()
+    {
+        System.out.println("Pick your move");
+        System.out.println("Basic Attack (1) \nAt the Ready(2)\nRedbull(3) \nBlock(4) \nMiss Travailer(5)");
+        int answer = s.nextInt();
+        switch (answer)
+        {
+            case 1:
+                moveOne("lol", 1);
+                break;
+            case 2:
+                moveTwo();
+                break;
+        }
+    }
+    public void takeDamage(int dam)
+    {
+        health -= dam;
+    }
+    public void missTravailer() // this is your ult
+    {
+
+    }
+    public void moveTwo()
+    {
+
     }
 }
